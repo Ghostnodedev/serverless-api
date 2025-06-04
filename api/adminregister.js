ca9108274578b42d2d8435ba0059c6fb6dfef581
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-06-04 14:23:07 +0530

    Initial commit

---
+++ api/adminregister.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const adata = [];
+  console.log(adata)
+
+  if (req.method === 'POST') {
+    const { name, username, email, password, businessName, businessType, mobile } = req.body;
+    console.log(name, username, email, password, businessName, businessType, mobile);
+    if(!name||!username ||!email ||!password ||!businessName ||!businessType ||! mobile){
+        res.status(400).send({message: 'Please fill in all fields.'})
+    }
+    adata.push({name,username, email,password,businessName,businessType,mobile})
+    console.log(adata)
+    res.status(200).json({ message: 'Data received', data: adata });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+};
