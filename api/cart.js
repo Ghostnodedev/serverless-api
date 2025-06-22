c5491e91667c2bfedde28f734fc12b726445a3c4
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-06-22 14:20:25 +0530

    Initial commit

--- api/cart.js
+++ api/cart.js
@@ -1,7 +1,13 @@
+"use strict"
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS ,GET");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   const { method, body } = req;
   console.log("Incoming data:", body);
 
