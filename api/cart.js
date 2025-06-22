0923a1c190ab0a3224eeb7eb7b872995e9e876a2
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-06-22 14:33:59 +0530

    Initial commit

--- api/cart.js
+++ api/cart.js
@@ -7,11 +7,27 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
-  
+
   const { method, body } = req;
   console.log("Incoming data:", body);
 
-  if (method === "GET") {
+ if (method === "POST") {
+    const { title, id, price, image } = body;
+
+    if (!title || !id || !price) {
+      return res.status(400).json({
+        message: "Please fill all the fields",
+      });
+    }
+
+    const postData = [{ title, id, price, image }];
+    console.log("Posted data:", postData);
+
+    return res.status(201).json({
+      message: "Data posted successfully",
+      data: postData,
+    });
+  } else if (method === "GET") {
     try {
       const url = "https://serverless-api-nine-dun.vercel.app/api/cart";
       const requestOptions = {
@@ -32,22 +48,6 @@ export default async function handler(req, res) {
       console.error("Fetch error:", error.message);
       res.status(500).json({ error: "Failed to fetch data" });
     }
-  } else if (method === "POST") {
-    const { title, id, price, image } = body;
-
-    if (!title || !id || !price) {
-      return res.status(400).json({
-        message: "Please fill all the fields",
-      });
-    }
-
-    const postData = [{ title, id, price, image }];
-    console.log("Posted data:", postData);
-
-    return res.status(201).json({
-      message: "Data posted successfully",
-      data: postData,
-    });
   } else {
     res.setHeader("Allow", ["GET", "POST"]);
     return res.status(405).json({ message: `Method ${method} not allowed` });
